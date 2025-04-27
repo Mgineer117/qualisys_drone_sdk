afaaf5222507cc9a77c56cc3b11cb551f34a0bc6
twhsu-stanley <twhsu@umich.edu>
2025-04-27 01:57:24 -0500

    fixed plotting: gyro is actually the same as attitude_rate

--- examples/flapper/plot_trajectory.py
+++ examples/flapper/plot_trajectory.py
@@ -8,7 +8,7 @@ import numpy as np
 import math
 
 # Load JSON file
-with open('examples/flapper/circular_traj/circular_Z_20250423190018.json', 'r') as f:
+with open('examples/flapper/circular_traj/circular_Z_20250423183719.json', 'r') as f:
     data = json.load(f)
 
 # Plot positions
@@ -137,42 +137,20 @@ axs[2].plot(controller_yaw, label='yaw ctrl reference')
 axs[2].legend()
 plt.show()
 
-# Plot body angular velocity 
+# Plot body angular velocity
+# NOTE:
+# stateCompressed.rateRoll = sensorData.gyro.x * deg2millirad;
+# stateCompressed.ratePitch = -sensorData.gyro.y * deg2millirad;
+# stateCompressed.rateYaw = sensorData.gyro.z * deg2millirad;
 gyro = data["gyro"] # [deg/s]
 gyro_x = np.array([c["gyro.x"] for c in gyro]) / 180 * math.pi # [rad/s]
 gyro_y = np.array([c["gyro.y"] for c in gyro]) / 180 * math.pi # [rad/s]
 gyro_z = np.array([c["gyro.z"] for c in gyro]) / 180 * math.pi # [rad/s]
 
-fig, axs = plt.subplots(3, 1, figsize=(8, 12))
-axs[0].plot(gyro_x, label='gyro_x')
-axs[0].legend()
-axs[1].plot(gyro_y, label='gyro_y')
-axs[1].legend()
-axs[2].plot(gyro_z, label='gyro_z')
-axs[2].legend()
-plt.show()
-
-attitude_rate_trans = np.zeros((state_roll.shape[0], 3))
-for i in range(state_roll.shape[0]):
-    roll_ = state_roll[i]
-    pitch_ = state_pitch[i]
-    R_hat = np.array([
-            [1, np.sin(roll_) * np.tan(pitch_), np.cos(roll_) * np.tan(pitch_)],
-            [0, np.cos(roll_), -np.sin(roll_)],
-            [0, np.sin(roll_) / np.cos(pitch_), np.cos(roll_) / np.cos(pitch_)]
-    ])
-    omega = np.array([gyro_x[i], gyro_y[i], gyro_z[i]])
-    attitude_rate_trans[i,:] = R_hat @ omega
-
-# Plot attitude rates
 attitude_rate = data["attitude_rate"] # [milliradians / sec]
 roll_rate = np.array([c["stateEstimateZ.rateRoll"] for c in attitude_rate]) / 1000 #[rad/s]
 pitch_rate = np.array([c["stateEstimateZ.ratePitch"] for c in attitude_rate]) / 1000 #[rad/s]
 yaw_rate = np.array([c["stateEstimateZ.rateYaw"] for c in attitude_rate]) / 1000 #[rad/s]
-
-roll_rate_deriv = np.gradient(state_roll, dt)
-pitch_rate_deriv = np.gradient(state_pitch, dt)
-yaw_rate_deriv = np.gradient(state_yaw, dt)
 
 controller_attitude_rate = data["controller_attitude_rate"] #[deg/s]
 controller_roll_rate = np.array([c["controller.rollRate"] for c in controller_attitude_rate]) / 180 * math.pi #[rad/s]
@@ -180,19 +158,58 @@ controller_pitch_rate = np.array([c["controller.pitchRate"] for c in controller_
 controller_yaw_rate = np.array([c["controller.yawRate"] for c in controller_attitude_rate]) / 180 * math.pi #[rad/s]
 
 fig, axs = plt.subplots(3, 1, figsize=(8, 12))
+axs[0].plot(gyro_x, label='gyro_x')
 axs[0].plot(roll_rate, label='roll_rate state')
 axs[0].plot(controller_roll_rate, label='roll_rate ctrl reference')
-#axs[0].plot(roll_rate_deriv, label='roll rate deriv')
-axs[0].plot(attitude_rate_trans[:,0], label='roll rate trans')
 axs[0].legend()
+axs[1].plot(-gyro_y, label='-gyro_y')
 axs[1].plot(pitch_rate, label='pitch_rate state')
 axs[1].plot(controller_pitch_rate, label='pitch_rate ctrl reference')
-#axs[1].plot(pitch_rate_deriv, label='pitch rate deriv')
-axs[1].plot(attitude_rate_trans[:,1], label='pitch rate trans')
 axs[1].legend()
+axs[2].plot(gyro_z, label='gyro_z')
 axs[2].plot(yaw_rate, label='yaw_rate state')
 axs[2].plot(controller_yaw_rate, label='yaw_rate ctrl reference')
-#axs[2].plot(yaw_rate_deriv, label='yaw rate deriv')
+axs[2].legend()
+plt.show()
+
+gyro_x_dot = np.gradient(gyro_x, dt) #[rad/s/s]
+gyro_y_dot = np.gradient(gyro_y, dt) #[rad/s/s]
+gyro_z_dot = np.gradient(gyro_z, dt) #[rad/s/s]
+
+fig, axs = plt.subplots(3, 1, figsize=(8, 12))
+axs[0].plot(gyro_x_dot, label='gyro_x_dot')
+axs[0].legend()
+axs[1].plot(gyro_y_dot, label='gyro_y_dot')
+axs[1].legend()
+axs[2].plot(gyro_z_dot, label='gyro_z_dot')
+axs[2].legend()
+plt.show()
+
+# Plot Roll_dot, pitch_dot, yaw_dot
+roll_rate_deriv = np.gradient(state_roll, dt)
+pitch_rate_deriv = np.gradient(state_pitch, dt)
+yaw_rate_deriv = np.gradient(np.unwrap(2 * state_yaw) / 2, dt)
+
+attitude_rate_trans = np.zeros((state_roll.shape[0], 3))
+for i in range(state_roll.shape[0]):
+    roll_ = state_roll[i]
+    pitch_ = state_pitch[i]
+    R_hat = np.array([
+        [1, np.sin(roll_) * np.tan(pitch_), np.cos(roll_) * np.tan(pitch_)],
+        [0, np.cos(roll_), -np.sin(roll_)],
+        [0, np.sin(roll_) / np.cos(pitch_), np.cos(roll_) / np.cos(pitch_)]
+    ])
+    omega = np.array([gyro_x[i], -gyro_y[i], gyro_z[i]])
+    attitude_rate_trans[i,:] = R_hat @ omega
+
+fig, axs = plt.subplots(3, 1, figsize=(8, 12))
+axs[0].plot(roll_rate_deriv, label='roll rate deriv')
+axs[0].plot(attitude_rate_trans[:,0], label='roll rate trans')
+axs[0].legend()
+axs[1].plot(pitch_rate_deriv, label='pitch rate deriv')
+axs[1].plot(attitude_rate_trans[:,1], label='pitch rate trans')
+axs[1].legend()
+axs[2].plot(yaw_rate_deriv, label='yaw rate deriv')
 axs[2].plot(attitude_rate_trans[:,2], label='yaw rate trans')
 axs[2].legend()
 plt.show()
